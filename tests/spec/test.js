acd67dfec9d58f335655d7579e3757ee822ae60e
Xotic750 <xotic750@gmail.com>
2015-12-17 13:32:27 +0100

    Update dependencies and docs

--- tests/spec/test.js
+++ tests/spec/test.js
@@ -3,10 +3,10 @@
 /*jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-  es3:true, esnext:false, plusplus:true, maxparams:1, maxdepth:1,
-  maxstatements:6, maxcomplexity:2 */
+  es3:true, esnext:false, plusplus:true, maxparams:1, maxdepth:2,
+  maxstatements:11, maxcomplexity:3 */
 
-/*global expect, module, require, describe, it, returnExports */
+/*global JSON:true, expect, module, require, describe, it, returnExports */
 
 (function () {
   'use strict';
@@ -14,6 +14,12 @@
   var isNil;
   if (typeof module === 'object' && module.exports) {
     require('es5-shim');
+    require('es5-shim/es5-sham');
+    if (typeof JSON === 'undefined') {
+      JSON = {};
+    }
+    require('json3').runInContext(null, JSON);
+    require('es6-shim');
     isNil = require('../../index.js');
   } else {
     isNil = returnExports;
