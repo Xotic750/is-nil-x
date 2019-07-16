63665bc7656f6447e6353a0bae476042c235c6ee
Graham Fairweather <xotic750@gmail.com>
2019-07-16 11:38:27 +0200

    :bookmark: v2.0.7
    Auto build

---
+++ __tests__/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    'jest/no-hooks': 'off',
+    'no-void': 'off',
+    'lodash/prefer-noop': 'off',
+    'compat/compat': 'off',
+  },
+};
--- dist/is-nil-x.js
+++ dist/is-nil-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-15T10:27:23.993Z",
+  "date": "2019-07-16T09:38:24.262Z",
   "describe": "",
   "description": "Checks if `value` is `null` or `undefined`.",
   "file": "is-nil-x.js",
   "hash": "444006902ff4284b2fe2",
   "license": "MIT",
-  "version": "2.0.6"
+  "version": "2.0.7"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
