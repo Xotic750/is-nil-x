c74f5a05511e2eeb8f7393330734d9236e25bd5e
Graham Fairweather <xotic750@gmail.com>
2019-07-18 17:53:05 +0200

    :bookmark: v2.0.13
    Full build run

--- dist/is-nil-x.js
+++ dist/is-nil-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-17T23:13:58.388Z",
+  "date": "2019-07-18T15:53:00.595Z",
   "describe": "",
   "description": "Checks if `value` is `null` or `undefined`.",
   "file": "is-nil-x.js",
   "hash": "e8f8e5d8c6aec6ca5db5",
   "license": "MIT",
-  "version": "2.0.12"
+  "version": "2.0.13"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
