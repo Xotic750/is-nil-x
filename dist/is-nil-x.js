379a950e41d5f64703cdb7905d4b6991d85ae82e
Graham Fairweather <xotic750@gmail.com>
2019-08-07 23:37:21 +0200

    Lodash security fix

--- dist/is-nil-x.js
+++ dist/is-nil-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-08-07T00:48:13.087Z",
+  "date": "2019-08-07T21:37:16.799Z",
   "describe": "",
   "description": "Checks if `value` is `null` or `undefined`.",
   "file": "is-nil-x.js",
